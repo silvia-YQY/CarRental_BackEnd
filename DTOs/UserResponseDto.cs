a74bbfbaaa47ea4bd62534e316c136e3536f73f8
silvia-YQY <545067444@qq.com>
2024-06-25 22:11:05 +1200

    fix: UserResponseDto add id

--- DTOs/UserResponseDto.cs
+++ DTOs/UserResponseDto.cs
@@ -2,6 +2,7 @@ namespace CarRentalPlatform.DTOs
 {
   public class UserResponseDto
   {
+    public required int Id { get; set; }
     public required string Username { get; set; } = "";
     public required string Email { get; set; } = "";
     public required bool isAdmin { get; set; }
